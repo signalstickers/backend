2c81b4fdd8fabd543f2ed1c78c83e4bc97f9a197
Romain Ricard <romain.ricard@mines-ales.org>
2021-02-17 19:25:19 +0100

    Move remove_empty_values to utils.py

--- signalstickers/apps/api/serializers/pack_serializer.py
+++ signalstickers/apps/api/serializers/pack_serializer.py
@@ -1,3 +1,4 @@
+from apps.api.utils import remove_empty_values
 from apps.stickers.models import Pack, Tag
 from rest_framework import serializers
 
@@ -17,17 +18,6 @@ class PackSerializer(serializers.ModelSerializer):
         fields = ("pack_id", "pack_key", "source", "tags", "nsfw", "original")
 
     def to_representation(self, obj):
-        def remove_empty_values(obj):
-            if isinstance(obj, dict):
-                out = {}
-                for key, value in obj.items():
-                    value = remove_empty_values(value)
-                    if remove_empty_values(value):
-                        out[key] = value
-                return out
-            if isinstance(obj, list):
-                return [remove_empty_values(value) for value in obj] or None
-            return obj
 
         return remove_empty_values(
             {
