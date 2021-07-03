77bdff31bc84ebd5e26472f9ea3771848255d886
Camille <camille.meunier21@gmail.com>
2021-07-03 15:44:06 +0200

    Add a RegexValidator on answer field
    In order to have answers that matches [a-zA-Z0-9]+,
    the answser field now validates that the value is valid.

--- signalstickers/apps/api/models/bot_prevention_questions.py
+++ signalstickers/apps/api/models/bot_prevention_questions.py
@@ -1,10 +1,17 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 
 class BotPreventionQuestion(models.Model):
 
     question = models.CharField(max_length=128)
-    answer = models.CharField(max_length=128)
+    answer = models.CharField(
+        max_length=128,
+        validators=[RegexValidator(
+            regex="^[a-zA-Z0-9]+$",
+            message="The answer must be alphanumeric -- No special characters allowed."
+        )]
+    )
 
     class Meta:
         db_table = "botpreventionquestions"
